c14c87bee53b628585946c628062c4dc9c3bcd47
guptaluv99 <guptaluv.8098@gmail.com>
2018-10-22 23:26:01 +0530

    changes in schedule

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let bot = require('./src/puppeteer');
 var schedule = require('node-schedule');
 let cnf = require('./config/config.json');
 
-var unfollow = schedule.scheduleJob('* * * *', function(){
+var unfollow = schedule.scheduleJob('0 0 0 * * *', function(){
   bot.doUnfollows();
 });
 
-var exploreHashTags = schedule.scheduleJob('* * * *', function(){
+var exploreHashTags = schedule.scheduleJob('0 0 * * * *', function(){
   bot.exploreHashTags();
 });
